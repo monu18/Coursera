1f025990811e60183d21bceccea52badb4e7dc93
monu <mozis.bot@gmail.com>
2021-03-01 20:46:10 +0530

    Python Programming: A Concise Introduction

--- Python-Programming/Problem1_1.py
+++ Python-Programming/Problem1_1.py
@@ -1,1 +1,2 @@
-s
+def problem1_1():
+    print ("Problem Set 1")
---
+++ Python-Programming/Problem1_3.py
@@ -0,0 +1,5 @@
+def problem1_3(n):
+    my_sum = 0
+    for number in range(n+1):
+        my_sum = my_sum + number
+    print (my_sum)
