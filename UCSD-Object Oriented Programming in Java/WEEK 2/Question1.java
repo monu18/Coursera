f7f77efb824a25a34698fce808d8bb45d73bdb13
monu <mozis.bot@gmail.com>
2021-03-18 12:31:17 +0530

    Objects, memory models, and scope

--- UCSD-Object Oriented Programming in Java/WEEK 2/Question1.java
+++ UCSD-Object Oriented Programming in Java/WEEK 2/Question1.java
@@ -1,17 +1,17 @@
-public class MyClass1
+public class Question1
 {
   private int a;
   public double b;
-  
-  public MyClass1(int first, double second)
+
+  public Question1(int first, double second)
   {
     this.a = first;
     this.b = second;
   }
   public static void main(String[] args)
   {
-    MyClass1 c1 = new MyClass1(10, 20.5);
-    MyClass1 c2 = new MyClass1(10, 31.5);
+    Question1 c1 = new Question1(10, 20.5);
+    Question1 c2 = new Question1(10, 31.5);
     System.out.println(c1.a + ", " + c1.b);
   }
 }
